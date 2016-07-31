05912e341a6e0d97384ada5e60e0a1db2a835b22
polarbear <15001725262@163.com>
2016-07-31 17:32:27 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+run :
+    @PORT=80 ./node_modules/.bin/nodemon ./bin/www
+.PHONY: run
---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+	token: '123456',
+	appid: 'wx7c2b06aa60d2e903',
+	encodingAESKey: 'C2ITdY1Xkjfm0SP8GhNavGK84xZDRl6lhzwKszpPR3y'
+};
+module.exports = config;
